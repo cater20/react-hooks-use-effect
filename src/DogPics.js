811be33bdbcd2e46271d2a94a30194f79d579ed2
chrisphine <onyangoketta@gmail.com>
2022-07-18 13:09:27 +0300

    setting dependency arrays

--- src/DogPics.js
+++ src/DogPics.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 // WARNING: this useEffect will run in an infinite loop!
 // to fix, pass an empty array as the second argument for useEffect
+
 function DogPics() {
   const [images, setImages] = useState([]);
 
@@ -10,12 +11,12 @@ function DogPics() {
     fetch("https://dog.ceo/api/breeds/image/random/3")
       .then((r) => r.json())
       .then((data) => {
-        console.log("setState");
+       // console.log("setState");
         setImages(data.message);
       });
   });
 
-  console.log("render");
+  //console.log("render");
 
   return (
     <div>
