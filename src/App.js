ff146db41b53f02121b4e4fe26f95687f9b15e12
chrisphine <onyangoketta@gmail.com>
2022-07-18 13:17:59 +0300

    done

--- src/App.js
+++ src/App.js
@@ -4,18 +4,17 @@ function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState("");
 
+ 
   useEffect(() => {
-    console.log("useEffect called");
-    fetch("https://dog.ceo/api/breeds/image/random/3")
-    .then((r) => r.json())
-    .then((data) => {
-      setImages(data.message);
-    });
-  },
-  []
-  );
+    document.title = text;
+  }, [text]);
+
+  useEffect(() => {
+    setTimeout(() => setCount(0), 5000);
+  }, []);
 
   console.log("Component rendering");
+ 
 
   return (
     <div>
